6423ab422d4280be6eb2d7652ca90605126e604f
14ec017 <pavithrap@acetcbe.edu.in>
2017-04-24 11:24:22 +0530

    Update checkno.c

--- checkno.c
+++ checkno.c
@@ -6,15 +6,12 @@ int a;
 clrscr();
 printf("\n Enter the no:");
 scanf("%d",&a);
-if(a!=0)
-{
- if(a>0)
+if(a>0)
  printf("\n Positive number");
  else if
  printf("\n Negative Number");
  else
  printf("\n The number is zero");
- }
- getch();
+getch();
  }
  

c97671bbf794d25e0f2c85f99e7bcac7a5426ea3
14ec017 <pavithrap@acetcbe.edu.in>
2017-04-24 11:29:46 +0530

    Create evenno.c

---
+++ evenno.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\nEnter the number:);
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Even number");
+}
+else
+{
+printf("Odd Number");
+}
+getch();
+}

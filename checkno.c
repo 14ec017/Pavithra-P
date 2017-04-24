026f28ad27f74035c21743fdedb32fb5b51b3549
14ec017 <pavithrap@acetcbe.edu.in>
2017-04-24 11:14:40 +0530

    Create checkno.c

---
+++ checkno.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n Enter the no:");
+scanf("%d",&a);
+if(a!=0)
+{
+ if(a>0):
+ printf("\n Positive number");
+ else if:
+ printf("\n Negative Number");
+ else
+ printf("\n The number is zero");
+ }
+ getch();
+ }
+ 

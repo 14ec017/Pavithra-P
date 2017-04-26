c24d7b1d4921fc0f5483d5ffedc262b68383fc91
14ec017 <pavithrap@acetcbe.edu.in>
2017-04-26 08:57:40 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,sum=0,n;
+printf("\n Enter the numbers:");
+scanf("%d",&n);
+for(i=0;i<=n;i++);
+{
+sum=sum+1;
+printf("\n%d",sum);
+}
+getch();
+}

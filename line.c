b929f05e9824bc8bab2e4a4e1e8da4befcd178e2
14ec017 <pavithrap@acetcbe.edu.in>
2017-06-28 16:34:53 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+int i=0,c=0;
+clrscr();
+printf("paragraph:");
+for(i=0;i<n;i++)
+{
+switch(a[i])
+{
+case '.':c++;break;
+case '!':c++;break;
+case '?':c++;break;
+case ';':c++;break;
+}}
+printf("%d",c);
+getch();
+}

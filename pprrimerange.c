2319f58a7ff56f30ecfd5e6a86ff19b3c703ee24
14ec017 <pavithrap@acetcbe.edu.in>
2017-06-30 11:28:31 +0530

    Create pprrimerange.c

---
+++ pprrimerange.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,flag=0,a,b;
+clrscr();
+scanf("%d %d",&a,&b);
+for(i=a;i<=b;i++)
+{
+flag=0;
+for(j=2;j<=i/2;j++)
+{
+if(i%j==0)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("%d\n",i);
+}}
+getch();
+}

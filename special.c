005784057f5f4fb1ce619da13b004c6103996f3c
14ec017 <pavithrap@acetcbe.edu.in>
2017-06-30 09:01:47 -0700

    Update special.c

--- special.c
+++ special.c
@@ -3,18 +3,26 @@
 #include<string.h>
 void main()
 {
-int d=0,l;
+int d=0,i=0,al=0,s=0;
 char a[50];
 clrscr();
 printf("Enter the string:");
 gets(a);
-l=strlen(a);
-for(i=0;i<=l;i++)
+while(a[i]!='\0')
 {
-if((a[i]!=' ')||(a[i]!>='0'&&a[i]!<='9')||(a[i]!>='a'&&a[i]!<='z')||(a[i]!>='A'&&a[i]!<='Z'))
+if((a[i]>='0')&&(a[i]<='9'))
 {
 d++;
+}
+else if(((a[i]>='a')&&(a[i]<='z'))||((a[i]>='A')&&(a[i]<='Z')))
+{
+  al++;
+}
+else
+{
+  s++;
 }}
-printf("Special:%d",d);
+  i++;
+printf("Special:%d",s);
 getch();
 }

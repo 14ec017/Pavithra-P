8dded569e64347d2708374df9927456b37903bd2
14ec017 <pavithrap@acetcbe.edu.in>
2017-06-30 15:21:58 +0530

    Update day.c

--- day.c
+++ day.c
@@ -7,13 +7,20 @@ clrscr();
 gets(a);
 switch(a)
 {
-case 'monday':true;break;
-case 'tuesday':true;break;
-case 'wednesday':true;break;
-case 'thrusday':true;break;
-case 'friday':true;break;
-case 'saturday':false;break;
-case 'sunday':false;break;
+case 'monday':true;
+                break;
+case 'tuesday':true; 
+                 break;
+case 'wednesday':true;
+                 break;
+case 'thrusday':true;
+                break;
+case 'friday':true;
+                 break;
+case 'saturday':false;
+                  break;
+case 'sunday':false;
+                break;
 }
 getch();
 }

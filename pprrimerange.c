d16e1401a1c9722a3d0756b1c971768b2c61a1e3
14ec017 <pavithrap@acetcbe.edu.in>
2017-06-30 11:29:28 +0530

    Update pprrimerange.c

--- pprrimerange.c
+++ pprrimerange.c
@@ -2,12 +2,13 @@
 #include<conio.h>
 void main()
 {
-int i,j,flag=0,a,b;
+int i,j,flag=0,a,b,count;
 clrscr();
 scanf("%d %d",&a,&b);
 for(i=a;i<=b;i++)
 {
 flag=0;
+  count++;
 for(j=2;j<=i/2;j++)
 {
 if(i%j==0)
@@ -17,7 +18,7 @@ break;
 }
 if(flag==0)
 {
-printf("%d\n",i);
+printf("%d\n",count);
 }}
 getch();
 }
